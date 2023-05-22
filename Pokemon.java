41ca5950736f7a2a331a59f31a8e58fcd5cd83e9
Taamim Naamid <tnaamid40@stuy.edu>
2023-05-22 14:51:45 -0400

    Barebones Pokemon class implemented, it is very dependent on other classes so there is not much there.

---
+++ Pokemon.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Pokemon {
+  // lets assume we go for the big pokemon info plan
+  private int currentHP;
+  // private int primaryType;
+  // private int secondaryType;
+  private int level;
+  private int exp;
+  // private int expCurve;
+  // private int evolutionLvl;
+  private String nickname;
+  // private String speciesName
+  // private int[] baseStats;
+  private Moves[] moves;
+  private int dexNumber;
+  public Pokemon(int currentHP, int level, int exp, String nickname, int dexNumber) {
+    this.currentHP = currentHP;
+    this.level = level;
+    this.exp = exp;
+    this.nickname = nickname;
+    this.dexNumber = dexNumber;
+  }
+  public int changeHP(int changeVal) {
+    int initial = currentHP;
+    // im going to assume we have a baseStats[] for maxHP
+    maxHP = baseStats[0];
+    currentHP+=changeVal;
+    currentHP = Math.min(maxHP,currentHP);
+    currentHP = Math.max(0,currentHP);
+    return initial;
+  }
+
+  //---------- STANDARD GET/SET METHODS BELOW ----------//
+  public int getCurrentHP() {
+    return currentHP;
+  }
+  public void setCurrentHP(int newHP) {
+    currentHP = newHP;
+  }
+  public int getLevel() {
+    return level;
+  }
+  public void setLevel(int newLevel) {
+    level = newLevel;
+  }
+  public int getExp() {
+    return exp;
+  }
+  public void setExp(int newExp) {
+    exp = newExp;
+  }
+  public String getNickname() {
+    return nickname;
+  }
+  public void setNickname(String newNickname) {
+    nickname = newNickname;
+  }
+  public int getDexNumber() {
+    return dexNumber;
+  }
+  public void setDexNumber(int newDexNumber) {
+    dexNumber = newDexNumber;
+  }
+  public Moves[] getMoves() {
+    return moves;
+  }
+  public void setMoves(Moves[] newMoves) {
+    moves = newMoves;
+  }
+}
