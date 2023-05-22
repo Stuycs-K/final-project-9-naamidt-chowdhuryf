92693d2e9eb75437ed8fd78f56fbf09f14da96bd
Taamim Naamid <tnaamid40@stuy.edu>
2023-05-22 14:58:51 -0400

    Added Pokedex and started to work on adding fields, not much done on that end

---
+++ Pokedex.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Pokedex {
+  private HashMap<Integer,String> speciesName;
+  private HashMap<Integer,int[]> baseStats;
+  private HashMap<Integer,PImage> sprite;
+  private HashMap<Integer,Integer> expCurve;
+  private HashMap<Integer,Integer> evolutionLvl;
+  private HashMap<Integer,Integer> evolution; // <-- returns dex # of evolution
+  private 
+}
