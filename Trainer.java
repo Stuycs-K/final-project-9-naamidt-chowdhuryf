c73945edf42e0e4d2efaa3e4086943f39e4a6b04
Taamim Naamid <tnaamid40@stuy.edu>
2023-05-25 14:58:37 -0400

    Was debugging things on the other side so not much done apart for a little bit of Trainer

---
+++ Trainer.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Trainer {
+  private String name;
+  private int[] position;
+  private int badges;
+  // private PImage sprite;
+  private Pokemon party;
+  // private Bag bag;
+  public Trainer(String name, int[] position, int badges) {
+    this.name = name;
+    this.position = position;
+    this.badges = badges;
+    party = new Pokemon[6];
+  }
+  public 
+}
